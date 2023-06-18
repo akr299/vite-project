02704fbed788ad55820a3faaf1315eb1daf9ee28
akr299 <akira.ohira@el-ltd.co.jp>
2023-06-18 15:11:20 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Sidebar from "./components/sidebar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
