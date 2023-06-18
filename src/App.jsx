1346bc3d92adf86b1de8fa722db3af4660d2752e
akr299 <akira.ohira@el-ltd.co.jp>
2023-06-18 15:24:44 +0900

    ノート追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import Sidebar from "./components/sidebar";
 import Main from "./components/Main";
+import { useState } from "react";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+  const onAddNote = () => {
+    console.log("新しくノートが追加されました");
+    const newNote = {
+      id: 1,
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes(newNote);
+    console.log(notes);
+  };
   return (
-    <div className="app">
-      <Sidebar />
+    <div className="App">
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
